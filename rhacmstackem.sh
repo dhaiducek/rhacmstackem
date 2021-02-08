d9166125e18d6f0a71b247cdd747269c3a1fe674
Dale Haiducek <dhaiduce@redhat.com>
2021-02-08 15:15:00 -0500

    Make rhacmstackem.sh executable

--- rhacmstackem.sh
+++ rhacmstackem.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+set -e
+
 # Clone requisite repos and store paths
 echo "$(date) ##### Cloning Lifeguard, Deploy, Pipeline, and StartRHACM repos"
 git clone https://github.com/dhaiducek/startrhacm.git
@@ -11,39 +13,56 @@ export LIFEGUARD_PATH=$(pwd)/lifeguard
 export RHACM_PIPELINE_PATH=$(pwd)/pipeline
 export RHACM_DEPLOY_PATH=$(pwd)/deploy
 
-# Login to ClusterPool cluster
-echo "$(date) ##### Logging in to ClusterPool cluster"
-oc login --token="${CLUSTERPOOL_HOST_TOKEN}" --server="${CLUSTERPOOL_HOST_API}" --insecure-skip-tls-verify=true
-
-# Setup Quay token for Deploy
-echo "$(date) ##### Setting up deploy Quay token"
-cat >${RHACM_DEPLOY_PATH}/prereqs/pull-secret.yaml <<EOF
-apiVersion: v1
-kind: Secret
-metadata:
-  name: multiclusterhub-operator-pull-secret
-data:
-  .dockerconfigjson: ${QUAY_PULL_SECRET}
-type: kubernetes.io/dockerconfigjson
-EOF
+# Check for Quay token for Deploy
+echo "$(date) ##### Checking for Quay token"
+if [[ -z "${QUAY_TOKEN}" ]]; then
+  echo "ERROR: QUAY_TOKEN not provided"
+  exit 1
+fi
 
 # ClusterClaim exports
 export CLUSTERPOOL_TARGET_NAMESPACE=${CLUSTERPOOL_TARGET_NAMESPACE:-"ERROR: Please specify CLUSTERPOOL_TARGET_NAMESPACE in environment variables"}
 export CLUSTERPOOL_NAME=${CLUSTERPOOL_NAME:-"ERROR: Please specify CLUSTERPOOL_NAME in environment variables"}
 export CLUSTERPOOL_RESIZE=${CLUSTERPOOL_RESIZE:-"true"}
 export CLUSTERPOOL_MAX_CLUSTERS=${CLUSTERPOOL_MAX_CLUSTERS:-"5"}
-export CLUSTERCLAIM_NAME="autoclaim-${CLUSTERPOOL_NAME}"
-export CLUSTERCLAIM_GROUP_NAME=${CLUSTERCLAIM_GROUP_NAME:-""}
+export CLUSTERCLAIM_NAME="rhacmstackem-${CLUSTERPOOL_NAME}"
+export CLUSTERCLAIM_GROUP_NAME=${CLUSTERCLAIM_GROUP_NAME:-"ERROR: Please specify CLUSTERCLAIM_GROUP_NAME in environment variables"}
 export CLUSTERCLAIM_LIFETIME=${CLUSTERCLAIM_LIFETIME:-"10h"}
 export AUTH_REDIRECT_PATHS=${AUTH_REDIRECT_PATHS:-()}
 
 # Run StartRHACM to claim cluster and deploy RHACM
 echo "$(date) ##### Running StartRHACM"
 ./startrhacm/startrhacm.sh
-echo "$(date) ##### Sending credentials to Slack"
-export KUBECONFIG=${LIFEGUARD_PATH}/clusterclaims/*/kubeconfig
-SNAPSHOT=$(oc get pod -l app=acm-custom-registry -o jsonpath='{.items[].spec.containers[0].image}' | grep -o "[0-9]\+\..*SNAPSHOT.*$")
-RHACM_URL=$(oc get routes multicloud-console -o jsonpath='{.status.ingress[0].host}')
-jq -r 'to_entries[] | "*\(.key)*: \(.value)"' ${LIFEGUARD_PATH}/clusterclaims/*/*.creds.json \
-| awk 'BEGIN{printf "{\"text\":\"*Snapshot*:'${SNAPSHOT}'\\n"};{printf "%s\\n", $0};END{printf "*RHACM URL*:'${RHACM_URL}'\\n\"}"}' \
-| curl -X POST -H 'Content-type: application/json' --data @- ${SLACK_URL}
+
+# Set up RBAC users
+if [[ "${RBAC_SETUP:-"true"}" == "true" ]]; then
+  echo "$(date) ##### Setting up RBAC users"
+  export RBAC_PASS=$(date | md5sum)
+  export KUBECONFIG=${LIFEGUARD_PATH}/clusterclaims/*/kubeconfig
+  touch ./rbac/htpasswd
+  for access in cluster ns; do
+    for role in cluster-admin admin edit view group; do
+      htpasswd -b ./rbac/htpasswd e2e-${role}-${access} ${RBAC_PASS}
+    done
+  done
+  oc create secret generic e2e-users --from-file=htpasswd=./rbac/htpasswd -n openshift-config || true
+  rm ./rbac/htpasswd
+  if [[ -z "$(oc -n openshift-config get oauth cluster -o jsonpath='{.spec.identityProviders}')" ]]; then
+    oc patch -n openshift-config oauth cluster --type json --patch '[{"op":"add","path":"/spec/identityProviders","value":[]}]'
+  fi
+  if [ ! $(oc -n openshift-config get oauth cluster -o jsonpath='{.spec.identityProviders[*].name}' | grep -o 'grc-e2e-htpasswd') ]; then
+    oc patch -n openshift-config oauth cluster --type json --patch "$(cat ./rbac/e2e-rbac-auth.json)"
+  fi
+  oc apply --validate=false -k ./rbac
+fi
+
+# Send cluster information to Slack
+if [[ -n "${SLACK_URL}" ]]; then
+  echo "$(date) ##### Sending credentials to Slack"
+  export KUBECONFIG=${LIFEGUARD_PATH}/clusterclaims/*/kubeconfig
+  SNAPSHOT=$(oc get pod -l app=acm-custom-registry -o jsonpath='{.items[].spec.containers[0].image}' | grep -o "[0-9]\+\..*SNAPSHOT.*$")
+  RHACM_URL=$(oc get routes multicloud-console -o jsonpath='{.status.ingress[0].host}')
+  jq -r 'to_entries[] | "*\(.key)*: \(.value)"' ${LIFEGUARD_PATH}/clusterclaims/*/*.creds.json \
+  | awk 'BEGIN{printf "{\"text\":\"*Snapshot*:'${SNAPSHOT}'\\n*RBAC Password*:'${RBAC_PASS}'\\n"};{printf "%s\\n", $0};END{printf "*RHACM URL*:'${RHACM_URL}'\\n\"}"}' \
+  | curl -X POST -H 'Content-type: application/json' --data @- ${SLACK_URL}
+fi
